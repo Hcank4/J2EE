f33a2358b9f775371e42697e6092da41f187bc99
Hcank4 <canpham522@gmail.com>
2026-03-12 11:09:37 +0700

    Update bài 6

---
+++ Code/Bai6/src/main/java/com/example/Lab6/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.Lab6.repository;
+
+import com.example.Lab6.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
